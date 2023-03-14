6cc4409bd8cbb55f6a3552feb8289a091c0a9322
bohdandev1997 <bbohdan1997@gmail.com>
2023-03-14 05:18:22 +0200

    Parse block, transaction to collect balances

---
+++ README.md
@@ -0,0 +1,7 @@
+Sript to find LP holders balances by given block number and smart contract address
+
+Script get exchange rate token to USDC on Coingecko by symbol which it gets 
+from given contract
+
+Output CSV file which contains addresses and balanses of all found holders
+
--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import csv
+import json
 import os
-
-from multiprocessing import Pool
+import requests
 
 from web3 import Web3
 
@@ -10,47 +10,75 @@ api_key = os.environ.get('ALCHEMY_API_KEY')
 alchemy_url = f"https://eth-mainnet.g.alchemy.com/v2/{api_key}"
 w3 = Web3(Web3.HTTPProvider(alchemy_url))
 
-members = {}
+
+# read abi from file
+with open('abi.json') as f:
+    ABI = json.load(f)
 
 
-def get_tx_info(tx):
-    tx_receipt = w3.eth.get_transaction_receipt(tx)
-    transaction = w3.eth.get_transaction(tx)
-    tx_value = transaction.get('value')
-    if tx_receipt.status == 1 and tx_value:
-        tx_from = str(tx_receipt.get('from'))
-        tx_to = str(tx_receipt.get('to'))
-        return tx_from, tx_to, tx_value
+def get_token_to_usdc_rate(symbol):
+    coins = requests.get("https://api.coingecko.com/api/v3/coins/list").json()
+    coin_id = ''
+    for coin in coins:
+        if symbol.lower() == coin['symbol']:
+            coin_id = coin['id']
+            break
+    if coin_id:
+        coin_info = requests.get(
+            f"https://api.coingecko.com/api/v3/coins/{coin_id}"
+        ).json()
+        coin_to_usd = coin_info["market_data"]["current_price"]["usd"]
+        usdc_info = requests.get(
+            f"https://api.coingecko.com/api/v3/coins/usd-coin"
+        ).json()
+        usdc_to_usd = usdc_info["market_data"]["current_price"]["usd"]
+        return coin_to_usd / usdc_to_usd
+    else:
+        print("Coin not found")
+        return
 
 
-def parse_liquidity_providers(block_number, address):
+def get_lp_transactions(block_number, address):
+    ''' Get transactions from block filtered by contract address '''
     if not w3.isConnected():
         print('Blockchain is not connected')
         return
-    
-    # while block_number <= w3.eth.block_number:
-    end_block = 16812520
-    tx_pool = []
-    while block_number < end_block:
-        block = w3.eth.get_block(block_number)
-        block_number += 1
-        for tx in block.transactions:
-            tx_pool.append(tx)
-        with Pool(10) as p:
-            txs = p.map(get_tx_info, tx_pool)
-        for tx in txs:
-            if tx and tx[1] == address:
-                if tx[1] in members:
-                    members[tx[0]] += tx[2]
-                else:
-                    members[tx[0]] = tx[2]
+    block = w3.eth.get_block(block_identifier=block_number)
+    lp_txs = []
+    for tx in block.transactions:
+        tx_receipt = w3.eth.get_transaction_receipt(tx)
+        if tx_receipt.to == w3.toChecksumAddress(address):
+            lp_txs.append(tx)
+    return lp_txs
 
-    with open('dict.csv', 'w') as csv_file:  
+
+def get_lp_providers_info(block, address):
+    ''' Find balance for LP liquidity providers '''
+    txs = get_lp_transactions(block_number=block, address=address)
+    members = {}
+    contract_instance = w3.eth.contract(
+        address=w3.toChecksumAddress(address), abi=ABI
+    )
+    symbol = contract_instance.functions.symbol().call()
+    decimals = contract_instance.functions.decimals().call()
+    exchange_rate = get_token_to_usdc_rate(symbol)
+    for tx in txs:
+        tx_info = w3.eth.get_transaction(tx)
+        provider = tx_info.get('from')
+        wei_balance = contract_instance.functions.balanceOf(provider).call()
+        balance = wei_balance / 10 ** decimals
+        if provider in members:
+            members[provider] += balance
+        else:
+            members[provider] = balance
+
+    with open(f"{symbol}-{block}.csv", "w") as csv_file:
         writer = csv.writer(csv_file)
         for key, value in members.items():
-            writer.writerow([key, value])
+            writer.writerow([key, str(value * exchange_rate)])
 
-parse_liquidity_providers(
-    block_number=16812510, 
-    address='0x6dfc34609a05bC22319fA4Cce1d1E2929548c0D7'
+
+get_lp_providers_info(
+    block=16787114, 
+    address='0xd533a949740bb3306d119cc777fa900ba034cd52'
 )
